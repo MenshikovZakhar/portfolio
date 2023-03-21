5085eb3892b4c3dd8e1cfc83ce06cdc626dbce88
Zakhar Menshikov <zm681986@mail.ru>
2023-03-21 22:12:38 +0500

    portfolio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './pages/index.css';
+
+
+
+
 function onEntry(entry) {
   entry.forEach(change => {
     if (change.isIntersecting) {
@@ -6,13 +10,43 @@ function onEntry(entry) {
     }
   });
 }
-
 let options = {
   threshold: [0.5]
 };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.item');
-
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+  animationTime = 450,
+  framesCount = 20;
+anchors.forEach(function (item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function (e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    // запускаем интервал, в котором
+    let scroller = setInterval(function () {
+      // считаем на сколько скроллить за 1 такт
+      let scrollBy = coordY / framesCount;
+
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+      // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
+});
