813cb0edaf7259f477d0b10e5733395a9c4c74e1
Zakhar Menshikov <zm681986@mail.ru>
2023-03-28 21:16:57 +0500

    portfolio

--- src/scripts/components/initialCards.js
+++ src/scripts/components/initialCards.js
@@ -1,31 +1,39 @@
-import slide1 from '../../images/css.png';
-
-
+import slide1 from '../../images/colorist-nextjs.png';
+import slide2 from '../../images/chan.png';
+import slide3 from '../../images/mesto-react.png';
+import slide4 from '../../images/russian-travel.png';
+import slide5 from '../../images/mesto.gif';
+import slide6 from '../../images/Lubimovka.png';
 export const initialCards = [
   {
-    name: "js",
+    name: "Colorist-nextjs",
     link: slide1,
-
+    adescription: "Сайт парикмахера-колориста"
   },
 
   {
-    name: "css",
-    link: slide1,
+    name: "Банные чаны",
+    link: slide2,
+    adescription: "Интернет магазин"
   },
   {
-    name: "css",
-    link: slide1,
+    name: "Mesto-react",
+    link: slide3,
+    adescription: "Одностраничное приложение"
   },
   {
-    name: "css",
-    link: slide1,
+    name: "Путешествие по России",
+    link: slide4,
+    adescription: "Landing page"
   },
   {
-    name: "css",
-    link: slide1,
+    name: "Mesto",
+    link: slide5,
+    adescription: "Учебный проект"
   },
   {
-    name: "css",
-    link: slide1,
+    name: "Любимовка",
+    link: slide6,
+    adescription: "Landing page"
   },
 ];
