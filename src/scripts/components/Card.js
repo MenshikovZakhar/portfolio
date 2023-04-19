4c00fc8b9212136669a340d05eb395323432b912
Zakhar Menshikov <zm681986@mail.ru>
2023-04-19 23:35:44 +0500

    portfolio

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -29,7 +29,8 @@ export default class Card {
     this._card.addEventListener('mouseover', () => this._handMouseover());
     this._card.addEventListener('mouseout', () => this._handleMouseout());
     this._card.addEventListener('touchstart', () => this._handMouseover());
-    this._card.addEventListener('touchhend', () => this._handleMouseout());
+    this._card.addEventListener('touchcancel', () => this._handleMouseout());
+
   };
 
   // методом, который добавляет данные в разметку
